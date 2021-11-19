28f36ac65010778d8a95c7b032869fc05af2b88f
jziwo2s <jimmytsajio97@yahoo.com>
2021-11-19 10:56:24 +0100

    Commit 10: add icon Home im Nav.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,36 @@
-let butn= document.getElementById('idbutton');
-let modal= document.getElementById('id01');
 
-//butn.addEventListener('click',myFunction);
-
+let modal1= document.getElementById('idpos1');
+let modal2= document.getElementById('idpos2');
+let modal3= document.getElementById('idwir');
+let modal4= document.getElementById('idrflex');
+let modal5= document.getElementById('idgrid');
+let modal6= document.getElementById('idlp');
 window.onclick = function (event){
-    if(event.target == modal){
-        modal.style.display="none";
+    if(event.target == modal1){
+        modal1.style.display="none";
+    }
+    if(event.target == modal2){
+        modal2.style.display="none";
+    }
+    if(event.target == modal3){
+        modal3.style.display="none";
+    }
+    if(event.target == modal4){
+        modal4.style.display="none";
+    }
+    if(event.target == modal5){
+        modal5.style.display="none";
+    }
+    if(event.target == modal6){
+        modal6.style.display="none";
     }
 }
 
-
-
-
-function changeColor(){
-    this.style.backgroundColor='#4c8';
-    //p3.style.color='red';
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += "responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
