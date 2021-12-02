3bb2406bf9bf18e4475d28f4e228711d9263a375
jziwo2s <jimmytsajio97@yahoo.com>
2021-12-02 16:10:00 +0100

    Commit 12: add von collapsible function zum lesen der DOMufgaben .

--- js/script.js
+++ js/script.js
@@ -52,7 +52,22 @@ const navSlide = () => {
         micon.classList.toggle('toggle');
     });
 
-
-
 }
 navSlide();
+
+//collapsible
+
+let collap = document.getElementsByClassName("collapsible");
+let i;
+
+for (i = 0; i< collap.length; i++ ){
+    collap[i].addEventListener("click", function () {
+        this.classList.toggle("collactive");
+        let content = this.nextElementSibling;
+        if(content.style.display==="block"){
+            content.style.display = "none";
+        }else {
+            content.style.display = "block";
+        }
+    });
+}
